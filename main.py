5d7461f05114544130a8ea540e99bba13b421c01
marisdanne <maris.danne@gamil.com>
2023-07-07 19:16:32 +0300

    dbadd

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///datubaze.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Persona(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    vards = db.Column(db.String(50))
+    epasts = db.Column(db.String(100), unique=True)
 
 @app.route('/')
 def home():
@@ -50,4 +60,7 @@ def post():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True, port=8080)
+    
